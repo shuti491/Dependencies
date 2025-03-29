40dd71a1d81783c87b7cf0dc95408c06a5355be9
shruti.singh <shruti.singh@servicenow.com>
2025-03-29 16:22:57 +0530

    adding text

--- PrjDep2/src/main/java/dep1/Dep2.java
+++ PrjDep2/src/main/java/dep1/Dep2.java
@@ -2,7 +2,7 @@ package dep1;
 
 public class Dep2 {
 	 public static void main(String[] args) {
-		 System.out.println("Dependency 2 is Loaded.....");
+		 System.out.println("Dependency  is Loaded.....");
 	 }
 	 
 
