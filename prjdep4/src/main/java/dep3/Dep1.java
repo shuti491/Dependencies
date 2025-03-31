09de27d788e9bf75c799f4384cbbd298364173df
shruti.singh <shruti.singh@servicenow.com>
2025-03-31 14:32:09 +0530

    push1

--- prjdep4/src/main/java/dep3/Dep1.java
+++ prjdep4/src/main/java/dep3/Dep1.java
@@ -2,7 +2,7 @@ package dep3;
 
 public class Dep1 {
 	 public static void main(String[] args) {
-		 System.out.println("Trial run Project 3 Dependency 1 is Loaded.....");
+		 System.out.println("Project 3 Dependency 1 is Loaded.....");
 	 }
 	 
 
