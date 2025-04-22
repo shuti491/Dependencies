d84841dfb9d0050273336c863cae445479a90f37
shuti491 <68851931+shuti491@users.noreply.github.com>
2025-04-22 16:06:35 +0530

    Update Dep1.java

--- PrjDep2/src/main/java/dep1/Dep1.java
+++ PrjDep2/src/main/java/dep1/Dep1.java
@@ -2,7 +2,7 @@ package dep1;
 
 public class Dep1 {
 	 public static void main(String[] args) {
-		 System.out.println("Project 1 Dependency 1 is Loaded.....");
+		 System.out.println("Project 1 Dependency 1 is Loaded..");
 	 }
 	 
 
